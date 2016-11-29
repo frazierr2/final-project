c87c04d32c7acc8c2315e87dec5df026ccda12f6
Ryan Frazier <frazierr2@gmail.com>
2016-11-29 09:43:23 -0500

    testing drop down on new page

--- app/scripts/templates/detailTemplate.jsx
+++ app/scripts/templates/detailTemplate.jsx
@@ -26,13 +26,13 @@ var DetailTemplate = React.createClass({
         <div className="navbar navbar-default hidden-sm hidden-md hidden-lg">
           <div className="navbar-header mobile-navbar">
             <a id="testing" className="navbar-brand logo testing" href="#landing/">U-Grub</a>
-            <a className="navbar-toggle"  data-toggle="collapse" data-target=".navbar-collapse">
+            <a className="navbar-toggle"  data-toggle="collapse" data-target="#navbar-button" aria-expanded="false" aria-controls="navbar">
               <span className="icon-bar"></span>
               <span className="icon-bar"></span>
               <span className="icon-bar"></span>
             </a>
           </div>
-          <div className="navbar-collapse collapse">
+          <div className="navbar-collapse collapse" id="navbar-button">
             <ul className="nav navbar-nav mobile-navbar">
               <li><a id="mobilefont1-3" className="nav-tabs" href="#landing/">HOME</a></li>
               <li><a id="mobilefont1-3" className="nav-tabs" href="#newpost/">ADD RESTAURANTS</a></li>
